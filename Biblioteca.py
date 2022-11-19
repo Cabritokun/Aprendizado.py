228d92feaa77049cbec3ed99e3941e25848919fd
Patrick Brito <118137492+Cabritokun@users.noreply.github.com>
2022-11-19 18:08:48 -0300

    Add files via upload

---
+++ Biblioteca.py
@@ -0,0 +1,15 @@
+#importação de matemática
+import math
+num = int(input('digite um numero: '))
+raiz = math.sqrt(num)
+#arredondando para cima
+print('a raiz de {} é igual a {}'.format(num, math.ceil(raiz)))
+
+#importação de número aleatório
+import random
+num = random.randint(1, 10)
+print('Seu numero aleatorio é {}'.format(num))
+
+#importação de emoji
+import emoji
+print(emoji.emojize("Olá, mundo :mushroom:"))
